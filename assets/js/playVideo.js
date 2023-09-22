e33b4ece5a3044c6227afd91fe9f165ea4c4da80
nickcool1901 <nickcool1901@icloud.com>
2023-09-22 21:41:05 +0300

    playPause

---
+++ assets/js/playVideo.js
@@ -0,0 +1,10 @@
+// var video = document.getElementById("myVideo");
+// var playButton = document.getElementById("playButton");
+
+//     function playPause() {
+//         if (video.paused) {
+//             playButton.style.display = "block";
+//         } else {
+//             playButton.style.display = "none";
+//         }
+//     }
--- assets/js/video-auto.js
+++ assets/js/video-auto.js
@@ -1,8 +1,8 @@
 const video = document.getElementById('myVideo');
+const playButton = document.getElementById('playButton');
 
 // Добавляем обработчик события 'loadedmetadata' для видео
 video.addEventListener('loadedmetadata', function() {
-    // Как только видео загрузится, начинаем его воспроизведение
     video.play();
 });
 
@@ -10,3 +10,28 @@ video.addEventListener('loadedmetadata', function() {
 if (video.readyState >= 2) {
     video.play();
 }
+
+function playPause() {
+  if (video.paused) {
+    video.play();
+    playButton.style.display = "none";
+  } else if (video.play){
+    // video.pause(); I tested and don't need
+    playButton.style.display = "block";
+    video.style.visibility = "hidden";
+  }
+}
+
+function playPauseButton() {
+if (video.paused) {
+    video.play();
+    playButton.style.display = "none";
+    video.style.visibility = "visible";
+} else if (video.play){
+    // video.pause(); I tested and don't need
+    playButton.style.display = "block";
+}
+}
+
+// Добавляем обработчик события клика на кнопку
+playButton.addEventListener('click', playPauseButton);
