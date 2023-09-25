ce998577abcacf56d3c9668b2cd3c685fd86a743
nickcool1901 <nickcool1901@icloud.com>
2023-09-25 20:00:08 +0300

    Nav for smaller screens and adapt img

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,43 +1,8 @@
-// Функция, которая будет преобразовывать навигацию в список
 function toggleMenu() {
-    const menu = document.querySelector('.menu ul');
-    const mobileMenu = document.querySelector('.mobile-menu');
-
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        // Если ширина окна меньше или равна 768px, создаем выпадающее меню
-        if (!mobileMenu) {
-            const mobileMenu = document.createElement('select');
-            mobileMenu.classList.add('mobile-menu');
-
-            const menuItems = menu.querySelectorAll('li');
-            menuItems.forEach((menuItem) => {
-                const option = document.createElement('option');
-                const link = menuItem.querySelector('a');
-                option.value = link.getAttribute('href');
-                option.textContent = link.textContent;
-                mobileMenu.appendChild(option);
-            });
-
-            mobileMenu.addEventListener('change', function () {
-                const selectedOption = this.options[this.selectedIndex];
-                if (selectedOption) {
-                    window.location.href = selectedOption.value;
-                }
-            });
-
-            menu.replaceWith(mobileMenu);
-        }
-    } else {
-        // Если ширина окна больше 768px, возвращаем нормальное меню
-        if (mobileMenu) {
-            mobileMenu.replaceWith(menu);
-        }
-    }
+  var leftMenu = document.querySelector('.left-menu');
+  if (leftMenu.style.left === '-50vw' || leftMenu.style.left === '') {
+    leftMenu.style.left = '0';
+  } else {
+    leftMenu.style.left = '-50vw';
+  }
 }
-
-// Вызываем функцию при загрузке страницы и при изменении размера окна
-window.addEventListener('load', toggleMenu);
-window.addEventListener('resize', toggleMenu);
-
-// Также вызываем функцию сразу после загрузки страницы, чтобы установить начальное состояние
-toggleMenu();
