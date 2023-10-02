eb11e05b187730deb8ab9447af6d8119485f882f
Nikita <128256104+nickcool1901@users.noreply.github.com>
2023-10-02 18:41:21 +0300

    worcs on safari

--- assets/js/video-auto.js
+++ assets/js/video-auto.js
@@ -13,38 +13,36 @@ if (video.readyState >= 2) {
 
 // Проверка на наличие строки 'Safari' в user agent
 if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-  // Этот браузер является Safari
-  console.log('This is Safari');
-  
+
+console.log('This is Safari');
 function playPause() {
   if (video.paused) {
-    playButton.style.display = "block";
-  } else {
     playButton.style.display = "none";
+  } else {
+    playButton.style.display = "block";
   }
+  return;
 }
 } else {
-  // Этот браузер не является Safari
-  console.log('This is not Safari');
-  
+
+console.log('This is not Safari');
 function playPause() {
   if (video.paused) {
-    playButton.style.display = "none";
-  } else {
     playButton.style.display = "block";
+  } else {
+    playButton.style.display = "none";
   }
 }
 }
 
-
-function playPauseButton() {
-  if (video.paused) {
-    video.style.visibility = "visible";
-    video.play();
-    playButton.style.display = "none";
-  } else {
-    playButton.style.display = "block";
-  }
-}
+// function playPauseButton() {
+//   if (video.paused) {
+//     video.style.visibility = "visible";
+//     video.play();
+//     playButton.style.display = "none";
+//   } else {
+//     playButton.style.display = "block";
+//   }
+// }
 
 playButton.addEventListener('onclick', playPauseButton);
