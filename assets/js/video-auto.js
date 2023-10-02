e598ceac1240a66228b2a4bd4753100d15dfc448
Nikita <128256104+nickcool1901@users.noreply.github.com>
2023-10-02 20:09:39 +0300

    comment

--- assets/js/video-auto.js
+++ assets/js/video-auto.js
@@ -1,19 +1,18 @@
 const video = document.getElementById('myVideo');
 const playButton = document.getElementById('playButton');
 
-// Добавляем обработчик события 'loadedmetadata' для видео
+// Add event for video
 video.addEventListener('loadedmetadata', function() {
     video.play();
 });
 
-// Если видео уже загружено, начнем его воспроизведение
+// If video is loaded, it starts
 if (video.readyState >= 2) {
     video.play();
 }
 
-// Проверка на наличие строки 'Safari' в user agent
-if (navigator.userAgent.indexOf('Safari') != -1 ) {
-
+// Check for 'Safari' in user agent
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
 console.log('This is Safari');
 function playPause() {
   if (video.paused) {
@@ -21,34 +20,32 @@ function playPause() {
   } else {
     playButton.style.display = "block";
   }
+}
   function playPauseButton() {
     if (video.paused) {
       video.play();
       playButton.style.display = "none";
     } else {
-      playButton.style.display = "block";
+      playButton.style.display = "none";
     }
   }
 }
-} else {
-
-console.log('This is not Safari');
+else {
+console.log('This is not Safari'); // works also with safari and i can't solve it
 function playPause() {
   if (video.paused) {
-    playButton.style.display = "block";
-  } else {
     playButton.style.display = "none";
+  } else {
+    playButton.style.display = "block";
   }
 }
 function playPauseButton() {
   if (video.paused) {
     video.play();
-    playButton.style.display = "block";
+    playButton.style.display = "none";
   } else {
     playButton.style.display = "none";
   }
 }
 }
-
-
 playButton.addEventListener('click', playPauseButton);
