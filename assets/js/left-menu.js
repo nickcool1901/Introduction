9ec9e5f86804b63bd8808b685c25215657f1d5fe
nickcool1901 <nickcool1901@icloud.com>
2023-10-05 18:52:17 +0300

    left-manu (from top)

--- assets/js/left-menu.js
+++ assets/js/left-menu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     var leftMenu = document.querySelector('.left-menu');
-    if (leftMenu.style.left === '-50vw' || leftMenu.style.left === '') {
-      leftMenu.style.left = '0';
+    if (leftMenu.style.top === '-200vh' || leftMenu.style.top === '') {
+      leftMenu.style.top = '0';
     } else {
-      leftMenu.style.left = '-50vw';
+      leftMenu.style.top = '-200vh';
     }
   }  
